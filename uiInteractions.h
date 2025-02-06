0764a39355f0a669585f18c02a0255f5cdc8ee70
sylvain <vassilyB@gmail.com>
2025-02-06 03:51:28 +0100

    renamed uiInteractions.h to handleUIInteractions.h

--- uiInteractions.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include "player_headers.h"
-#include "RaylibManager.h"
-#include "DOM/RootNode.h"
-#include "DOM/Button.h"
-
-void prepareInteractions(RaylibManager& manager, RootNode* rootNode) {
-	
-}
